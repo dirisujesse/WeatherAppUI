4bf0b736a6875fba34d15868ef6e55a6e34f6e90
Jesse Dirisu <dirisujesse@gmail.com>
2023-05-02 16:48:51 +0100

    Initial Commit

---
+++ WeatherApp/WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppApp.swift
+//  WeatherApp
+//
+//  Created by Dirisu on 02/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
